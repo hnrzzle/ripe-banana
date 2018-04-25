1b2120e92e1e79166f4e9094e427320dada9d686
hnrzzle <henrique.j.oliveira1@gmail.com>
2018-04-25 09:40:42 -0700

    reviewer hash good

--- test/unit/Reviewer.test.js
+++ test/unit/Reviewer.test.js
@@ -4,17 +4,21 @@ const { getErrors } = require('./helpers');
 
 describe('Reviewer Model', () => {
 
+    const password = 'abc';
+
+
     it('Valid good model', () => {
         const data = {
             name: 'Angry Donald',
-            company: 'angrydonald.com'
+            company: 'angrydonald.com',
+            roles: []
         };
         const don = new Reviewer(data);
         data._id = don._id;
 
         assert.deepEqual(don.toJSON(), data);
     });
-
+    
     it('required fields', () => {
         const reviewer = new Reviewer({});
         const errors = getErrors(reviewer.validateSync(), 4);
@@ -23,5 +27,25 @@ describe('Reviewer Model', () => {
         assert.equal(errors.email.kind, 'required');
         assert.equal(errors.hash.kind, 'required');
     });
+    
+    it('generates hash from password and checks password', () => {
+        const data = {
+            name: 'Angry Donald',
+            company: 'angrydonald.com',
+            roles: []
+        };
+        const don = new Reviewer(data);
+        data._id = don._id;
+    
+        assert.deepEqual(don.toJSON(), data);
+
+        don.generateHash(password);
+        assert.ok(don.hash);
+        assert.notEqual(don.hash, password);
+
+        assert.isOk(don.comparePassword(password));
+
+    });
+
 
 });
